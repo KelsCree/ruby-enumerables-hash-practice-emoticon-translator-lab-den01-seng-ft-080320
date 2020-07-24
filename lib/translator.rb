0799e8026a766ebf08f47fd4506202d7ca8f3513
Kelsey Creehan <kelseycreehan@gmail.com>
2020-07-24 01:32:01 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -26,7 +26,7 @@ def get_japanese_emoticon(file_path, emoji)
      "Sorry, that emoticon was not found"
 end
 
-def get_english_meaning
+def get_english_meaning(file_path, emoji)
    library = load_library(file_path)
    library.each do |meaning, symbol|
      if emoji == symbol[:japanese]
