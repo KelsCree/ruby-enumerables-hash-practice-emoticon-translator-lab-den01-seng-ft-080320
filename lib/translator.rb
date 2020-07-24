1e68a2ead7393f7a60980ef8c447f81354844f2a
Kelsey Creehan <kelseycreehan@gmail.com>
2020-07-24 00:58:33 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,20 @@
 # require modules here
+require 'yaml'
+require 'pry'
 
-def load_library
-  # code goes here
+
+def load_library(file_path)
+  data = YAML.load_file(file_path)
+  result = data.each_with_object({}) do |(key, value), final_hash|
+    if !final_hash[key]
+      final_hash[key] = {
+        :english => value[0],
+        :japanese => value[1]
+      }
+    end
+  end
+    result
+  binding.pry
 end
 
 def get_japanese_emoticon
